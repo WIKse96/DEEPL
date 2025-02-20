bbf81d015f7a14b0a51d121148d2a1d1205bf297
WIKse96 <105929893+WIKse96@users.noreply.github.com>
2025-02-20 10:38:16 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,56 @@
+import sys
+import subprocess
+import datetime
 import deepl
+import os
 import pandas as pd
 
-df = pd.read_csv('./files/1.csv')
+directory = './przetlumaczone'
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
-#DANE DO LOGOWANIA DEEPL UKRYTE
+# Lista wymaganych pakietów
+required_packages = ["deepl", "pandas"]
 
-# Tworzenie DataFrame (df) lub załaduj go z pliku CSV
+# Sprawdzanie i instalacja brakujących pakietów
+for package in required_packages:
+    try:
+        __import__(package)
+    except ImportError:
+        print(f"Instalowanie pakietu {package}...")
+        install(package)
 
+if not os.path.exists(directory):
+    os.makedirs(directory)
+#definiowanie zmiennych
+
+origin_path = input("Podaj ścieżkę bezwzględną pliku do tłumaczenia (!!csv!!): ")
+lang_code = input("Podaj kod języka, na jaki tłumaczysz (EN-GB lub DE): ")
+lang_code_upper = lang_code.upper()
+store_code = ''
+
+if lang_code_upper == 'EN-GB':
+    store_code = 'en'
+elif lang_code_upper == 'DE':
+    store_code = 'de'
+else:
+    store_code = 'NIEZNANY - UZUPEŁNIJ!'
+
+
+
+input("Przetłumaczony plik w: ./przetlumaczone/")
+
+
+# tutaj nazwa pliku, który tłumaczysz
+df = pd.read_csv(origin_path )
+
+# Dodaj pustą kolumnę 'translated'
+df["translated"] = ""
+# Dodaj kolumnę uzupełnioną kodem sklepu
+df["store_view_code"] = "en"
+
+# DANE DO LOGOWANIA DEEPL UKRYTE
+translator = deepl.Translator("63e31250-4ff9-159e-5ab3-3569a31a210e:fx")
 
 j = 0
 while j < len(df):
@@ -31,8 +75,8 @@ while j < len(df):
 
         try:
             # Tłumaczenie części tekstu
-            #Do zmiany języka docelowego zmień `target_nag`
-            result = translator.translate_text(to_translate, target_lang="DE")
+            # TARGET_LANG - PODAJESZ KOD TŁUMACZENIA JEZYKA - DOCELOWY
+            result = translator.translate_text(to_translate, target_lang=lang_code_upper)
             translated_text = result.text
 
             # Połącz przetłumaczony tekst z pozostałymi częściami
@@ -62,5 +106,19 @@ while j < len(df):
 
     j += 1
 
+current_time = datetime.datetime.now()
+# Pobranie poszczególnych elementów bez wiodących zer
+year = current_time.strftime('%y')  # dwucyfrowy rok
+month = current_time.month          # miesiąc bez wiodącego zera
+day = current_time.day              # dzień bez wiodącego zera
+hour = current_time.hour            # godzina bez wiodącego zera
+minute = current_time.minute        # minuta bez wiodącego zera
+
+# Łączenie w żądanym formacie
+formatted_time = f'{year}{month}{day}-{hour}{minute}'
+
 # Zapisz DataFrame do pliku CSV
-df.to_csv('./files/1_updated.csv', index=False)
+df.to_csv(f'{directory}/updated-{formatted_time}.csv', index=False)
+print('\n')
+print("powered by Wiktor Ćwiertnia v1")
+input("wcisnij przynisk zeby zakonczyc")
